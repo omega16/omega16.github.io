a3b84bc123a7b0dd5bfbd7587a30d446ab81e512
omega16 <omega16@Yuki.N.Linux>
2019-10-17 01:21:30 -0500

    added top labels to all h2 in body

--- gh-pages/scripts/main.js
+++ gh-pages/scripts/main.js
@@ -1,1 +1,20 @@
-console.log("hi");
+
+
+function add_h_links(collection){
+    for (i = 0; i < collection.length; i++) {
+        h=collection.item(i)
+        var link = document.createElement("a");
+        link.appendChild(document.createTextNode("top"));
+        link.href = "#TOC";
+        link.className+=" top-link";
+        h.appendChild(link);
+    }
+    
+}
+
+
+var blog_entry = document.getElementsByClassName("blog-entry")[0];
+
+var blog_h2 = blog_entry.getElementsByTagName("h2");
+
+add_h_links(blog_h2)
